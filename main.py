69187c00e05e7c42c532229215c78b0386224126
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-03-15 06:32:25 +0900

    add random.go

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import random
-
-f = open("./artist_list.csv").read().split("\n")
-
-random.shuffle(f)
-
-print f[0]
+from flask import Flask
+app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    f = open("./result.txt").read().split("\n")
+    random.shuffle(f)
+    return f[0]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
